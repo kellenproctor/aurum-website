c53deb2fb9bd816735cac58aaf48368d5a3c1735
Kellen Proctor <kellenproctor@gmail.com>
2018-03-26 16:16:14 -0700

    FEAT: fix navbar

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,6 +1,13 @@
 var navButton = document.querySelector('button[aria-expanded]');
 
 function toggleNav({ target }) {
+    if (navButton.innerHTML == 'X') {
+        navButton.innerHTML = '&#9776;'
+    }
+    else {
+        navButton.innerHTML = 'X'
+    }
+
     const expanded = target.getAttribute('aria-expanded') === 'true' || false;
     navButton.setAttribute('aria-expanded', !expanded);
 }
